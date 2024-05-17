a2e148f360da14303cbf210115ca51f6c9a4bca8
hammad_ahmad <h.hammad2066@gmail.com>
2024-05-17 10:07:16 +0500

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,10 +1,23 @@
-import express from 'express';
+import express from "express";
 const app = express();
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
 
-app.get('/', (req, res) => {
-    res.send('hey')
-})
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_STRING)
+  .then(() => {
+    console.log(`Mongodb is connected`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/", (req, res) => {
+  res.send("hey");
+});
 
 app.listen(3000, () => {
-    console.log(`Server is running on port 3000`);
-})
+  console.log(`Server is running on port 3000`);
+});
