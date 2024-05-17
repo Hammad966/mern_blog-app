f47cdae4f4970f0d8d9a7a0bf4345d7c570f6f27
hammad_ahmad <h.hammad2066@gmail.com>
2024-05-17 17:05:22 +0500

    signup api

--- api/index.js
+++ api/index.js
@@ -1,15 +1,25 @@
 import express from "express";
 const app = express();
-app.use(express.json())
-import dotenv from 'dotenv';
-import userRoutes from './routes/user.route.js'
-import authRoutes from './routes/auth.route.js'
-
+app.use(express.json()); 
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+const port = 8000;
 dotenv.config();
- 
+
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
-app.listen(8001, () => {
-  console.log(`Server is running on port 3000`);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
